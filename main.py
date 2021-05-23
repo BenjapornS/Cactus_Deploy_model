859fda5c288aef722a0866f9803873d9951fa631
BenjapornS <Benjaporn.suw7@gmail.com>
2021-05-23 17:07:45 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+''' Deploy Model with Heroku '''
+import json
+import joblib
+import numpy as np
+import uvicorn
+from fastapi import FastAPI, Form
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from sklearn.ensemble import RandomForestClassifier
+
+app = FastAPI()
+
+@app.get("/")
+async def main():
+    return 'Deploy Model Success!'
+
+@app.get("/predict")
+async def create_item(humidity: float, temp: float, soil: float):
+    model = joblib.load('cactus.h6')
+    predicted = model.predict([[humidity, temp, soil]])
+    predicted = predicted.tolist()
+    response = jsonable_encoder({"result": predicted[0], "procId": "Cactus", 'complete': True})
+    return predicted[0]
+
+# if __name__ == '__main__':
+#     uvicorn.run(app, host="localhost", port=8000, debug=True)
+
+# http://localhost:8000/predict?humidity=61.33&temp=26.64&soil=35.47
